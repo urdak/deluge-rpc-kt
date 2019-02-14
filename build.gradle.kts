c24a8cfd5651584fcc0887831737792c9c70b706
Victor Merkulov <vic@ickis.net>
2019-02-14 01:02:16 +0100

    setup bintray publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import java.util.Properties
+import java.util.Date
+import java.nio.file.Files
+import com.jfrog.bintray.gradle.BintrayExtension.PackageConfig
+import com.jfrog.bintray.gradle.BintrayExtension.VersionConfig
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "net.ickis"
@@ -5,6 +11,9 @@ version = "0.1-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.3.20"
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
+    id("org.jetbrains.dokka") version "0.9.17"
 }
 
 allprojects {
@@ -34,4 +43,60 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+/* ------------------------- PUBLISHING ------------------------- */
+
+val sourcesJar = task<Jar>("sourcesJar") {
+    dependsOn(tasks["classes"])
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+}
+
+tasks.withType<DokkaTask> {
+    reportUndocumented = false
+    outputFormat = "javadoc"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val javadocJar = task<Jar>("javadocJar") {
+    dependsOn("dokka")
+    classifier = "javadoc"
+    from(buildDir.resolve("javadoc"))
+}
+
+publishing {
+    publications {
+        create("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+}
+
+val credentials = Files.newInputStream(rootProject.buildFile.toPath().resolveSibling("local.properties")).use {
+    val props = Properties()
+    props.load(it)
+    props
+}
+
+bintray {
+    user = credentials["bintray.user"] as? String
+    key = credentials["bintray.apikey"] as? String
+    pkg(closureOf<PackageConfig> {
+        repo = "maven"
+        name = project.name
+        setLicenses("MIT")
+        version(closureOf<VersionConfig> {
+            name = project.version.toString()
+            released = Date().toString()
+        })
+        setPublications("mavenJava")
+        vcsUrl = "https://github.com/urdak/deluge-rpc-kt"
+        publish = true
+    })
 }
