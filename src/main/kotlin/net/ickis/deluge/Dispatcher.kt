f5cea8006692257e46ca2dcd2d1798e25083e070
Victor Merkulov <vic@ickis.net>
2019-03-08 02:01:37 +0100

    Dispatcher documentation

--- src/main/kotlin/net/ickis/deluge/Dispatcher.kt
+++ src/main/kotlin/net/ickis/deluge/Dispatcher.kt
@@ -54,6 +54,9 @@ internal data class Subscribe<T>(
  * An incoming response command, containing the parsed [response] that is received from the daemon.
  */
 internal data class Receive(val response: DelugeResponse) : DispatcherCommand() {
+    /**
+     * Processes the response depending on its type.
+     */
     suspend fun process(
             sentMap: MutableMap<Int, Send<*>>,
             subscribedMap: MutableMap<String, List<Subscribe<*>>>
@@ -65,6 +68,11 @@ internal data class Receive(val response: DelugeResponse) : DispatcherCommand() 
         }.exhaustive
     }
 
+    /**
+     * Processes the response as a value. Attempts to create a responses type and complete the deferred.
+     * If the response creation throws an exception, the deferred is completed exceptionally using that exception.
+     * Ignores the response if the request is not found in the [sentMap].
+     */
     private fun DelugeResponse.Value.processValue(sentMap: MutableMap<Int, Send<*>>) {
         val sendCommand = sentMap.remove(requestId)
         if (sendCommand != null) {
@@ -82,6 +90,10 @@ internal data class Receive(val response: DelugeResponse) : DispatcherCommand() 
         }
     }
 
+    /**
+     * Processes the response as an error. Completes the deferred exceptionally using the exception from the error.
+     * Ignores the response if the request is not found in the [sentMap].
+     */
     private fun DelugeResponse.Error.processError(sentMap: MutableMap<Int, Send<*>>) {
         val sendCommand = sentMap.remove(requestId)
         if (sendCommand != null) {
@@ -91,6 +103,14 @@ internal data class Receive(val response: DelugeResponse) : DispatcherCommand() 
         }
     }
 
+    /**
+     * Processes the response as an event. Attempts to create notifications and send them to subscribed channels.
+     * If the notification creation throws an exception, the channel is closed using that exception and removed from the
+     * subscriptions.
+     * If the channel is closed for receive (the user closed the channel), the channel is also removed from the
+     * subscriptions.
+     * Ignores the response if the subscription is not found in the [subscribedMap].
+     */
     private suspend fun DelugeResponse.Event.processEvent(subscribedMap: MutableMap<String, List<Subscribe<*>>>) {
         val subscriptions = subscribedMap[eventName]
         if (subscriptions.isNullOrEmpty()) {
@@ -118,6 +138,10 @@ internal data class Receive(val response: DelugeResponse) : DispatcherCommand() 
     }
 }
 
+/**
+ * Creates a dispatcher that keeps track of ongoing requests and subscriptions.
+ * @param socket The socket that is used to send data with the [Send] command.
+ */
 internal fun CoroutineScope.dispatcher(socket: DelugeSocket) = actor<DispatcherCommand> {
     val sentMap = HashMap<Int, Send<*>>()
     val subscribedMap = HashMap<String, List<Subscribe<*>>>()
